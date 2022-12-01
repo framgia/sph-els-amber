060863cbb90027bd0d11d9aeef929c5d9fb97916
Nicole Amber Mariscal <116238730+nicoleamber@users.noreply.github.com>
2022-12-01 17:26:51 +0800

    Task 2.4: Answer functionality

--- backend/main/serializers/answer_serializer.py
+++ backend/main/serializers/answer_serializer.py
@@ -1,14 +1,25 @@
 from rest_framework import serializers
 from ..models.answer import Answer
+from ..models.choice import Choice
+from ..models.question import Question
 
 class AnswerSerializer(serializers.ModelSerializer):
-    user_id = serializers.PrimaryKeyRelatedField(many=False, read_only=True)
-    question_id = serializers.PrimaryKeyRelatedField(many=False, read_only=True)
-    lesson_id = serializers.PrimaryKeyRelatedField(many=False, read_only=True)
-    choice_id = serializers.PrimaryKeyRelatedField(many=False, read_only=True)
-
     class Meta:
         model = Answer
         fields = '__all__'
 
+    def create(self, validated_data):
+        choice = Choice.objects.get(pk=self.data['choice'])
+        question = Question.objects.get(pk=self.data['question'])
+        is_correct = True if question.correct_answer == choice.value else False
 
+        answer = Answer(
+            user=validated_data['user'],
+            question=validated_data['question'],
+            lesson=validated_data['lesson'],
+            choice=validated_data['choice'],
+            is_correct=is_correct,
+        )
+        
+        answer.save()
+        return answer
--- backend/main/views/answer.py
+++ backend/main/views/answer.py
@@ -1,12 +1,30 @@
 from rest_framework import generics
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 from ..serializers.answer_serializer import AnswerSerializer
 from ..models.answer import Answer
 
-class AnswerList(generics.ListAPIView):
-    queryset = Answer.objects.all()
-    serializer_class = AnswerSerializer
-    filterset_fields = ['user_id', 'lesson_id']
+class AnswerList(APIView):
+    def get(self, request):
+        answers = Answer.objects.all()
+        serializer = AnswerSerializer(answers, many=True)
 
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = AnswerSerializer(data=request.data)
+
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+
+            response = Response('Answer added', status=status.HTTP_201_CREATED)
+        else:
+            response =  Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+        return response
+           
+  
 class AnswerDetail(generics.RetrieveAPIView):
     queryset = Answer.objects.all()
     serializer_class = AnswerSerializer
