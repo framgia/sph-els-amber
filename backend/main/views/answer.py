7a9e996e0b0d60c852e30a94d10ab791a9d3e5ea
Nicole Amber Mariscal <116238730+nicoleamber@users.noreply.github.com>
2022-12-02 11:43:25 +0800

    [SELS-TASK] Answer Functionality (#6)
    * serializer issue
    
    * Task 2.3: Retrieve Quiz Endpoints
    
    * Task 2.3 [Hotfix]: Added '/' on answers endpoint
    
    * Task 2.4: Answer functionality
    
    * removed if statement from backend/main/views/answer.py
    
    * updated answer view and serializer

--- backend/main/views/answer.py
+++ backend/main/views/answer.py
@@ -1,12 +1,26 @@
 from rest_framework import generics
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 from ..serializers.answer_serializer import AnswerSerializer
 from ..models.answer import Answer
 
-class AnswerList(generics.ListAPIView):
-    queryset = Answer.objects.all()
-    serializer_class = AnswerSerializer
-    filterset_fields = ['user_id', 'lesson_id']
+class AnswerList(APIView):
+    def get(self, request):
+        answers = Answer.objects.all()
+        serializer = AnswerSerializer(answers, many=True)
 
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = AnswerSerializer(data=request.data)
+
+        serializer.is_valid(raise_exception=True)
+        serializer.save(serializer.validated_data)
+
+        return Response('Answer added', status=status.HTTP_201_CREATED)
+           
+  
 class AnswerDetail(generics.RetrieveAPIView):
     queryset = Answer.objects.all()
     serializer_class = AnswerSerializer
