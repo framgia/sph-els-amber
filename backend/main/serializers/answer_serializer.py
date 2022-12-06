9ff4ec7f8d86b52094960a074b6a08ad2c817d22
Nicole Amber Mariscal <116238730+nicoleamber@users.noreply.github.com>
2022-12-06 11:51:51 +0800

    updated answer_serializer.py; added new line to lesson and question reducers

--- backend/main/serializers/answer_serializer.py
+++ backend/main/serializers/answer_serializer.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 from ..models.answer import Answer
 from ..models.choice import Choice
-from ..models.question import Question   
+from ..models.question import Question
 
 class AnswerSerializer(serializers.ModelSerializer):
     choice_string = serializers.SerializerMethodField('get_choice_string')
@@ -13,7 +13,7 @@ class AnswerSerializer(serializers.ModelSerializer):
     def get_choice_string(self, obj):
         method = self.context['request'].method
         if method == 'GET':
-            choice = Choice.objects.get(pk=getattr(obj, 'choice_id'))
+            choice = Choice.objects.get(pk=obj.choice_id)
             return choice.value
 
     def save(self, validated_data):
