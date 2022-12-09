dd3072f5bcbd28f87ed62a416d2260736dd0fa83
Nicole Amber Mariscal <116238730+nicoleamber@users.noreply.github.com>
2022-12-09 14:59:40 +0800

    Task 4.1: Retrieve User and Activity Log CRUD

---
+++ backend/main/views/user.py
@@ -0,0 +1,11 @@
+from rest_framework import generics
+from ..serializers.user_serializer import UserSerializer
+from ..models.user import User
+
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
