3fb405f156b147aa4031b3e288441de7ff1bc52b
Nicole Amber Mariscal <116238730+nicoleamber@users.noreply.github.com>
2022-12-13 15:26:09 +0800

    [SELS-TASK] Register, Update & Delete User Endpoints (#14)
    * Task 5.2 - Register, Update & Delete User Endpoints
    
    * removed breakpoint and trailing space in password_serializer

--- backend/main/models/user.py
+++ backend/main/models/user.py
@@ -1,8 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.conf import settings
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from rest_framework.authtoken.models import Token
 
 class User(AbstractUser):
-    name = models.CharField(max_length=255)
+    username = models.CharField(max_length=255, unique=True)
     email = models.EmailField(max_length=255, unique=True)
     password = models.CharField(max_length=255)
     avatar = models.ImageField(null=True, blank=True)
@@ -11,7 +15,12 @@ class User(AbstractUser):
     updated_at = models.DateTimeField(auto_now_add=True)
     is_deleted = models.BooleanField(default=False)
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['name']
+    REQUIRED_FIELDS = ['username']
     
     def __str__(self):
-        return self.name
+        return self.username
+
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def create_auth_token(sender, instance=None, created=False, **kwargs):
+    if created:
+        Token.objects.create(user=instance)
